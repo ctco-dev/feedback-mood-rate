7a15473a8dc35583116a08c9c761d1d396ffd233
Vadim Germanenko <vadigerman@gmail.com@>
2018-08-27 16:10:46 +0300

    fixes

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,36 @@
+function login() {
+    hideError();
+    console.log("start login");
+    var usernameTxt = document.getElementById("username-txt");
+    var passwordTxt = document.getElementById("password1-txt");
+    var dto = {
+        "username": usernameTxt.value,
+        "password": passwordTxt.value
+    };
+    console.log("sending login data");
+    fetch('/api/auth/login', {
+        "method": "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dto)
+    }).then(function (response) {
+        if (response.status === 200) {
+            console.log("login success");
+            location.href = "../app/option.jsp";
+        } else {
+            showError("Username or Password is incorrect!");
+        }
+    })
+}
+
+function hideError() {
+    var errorPanel = document.getElementById("error-panel");
+    errorPanel.classList.add("w3-hide");
+}
+function showError(msg) {
+    var errorPanel = document.getElementById("error-panel");
+    errorPanel.classList.remove("w3-hide");
+    w3DisplayData("error-panel", {"message" : msg});
+}
