c43d94be087643d158b572eda54709e2f45cbebb
Margarita Sankova <margarita.sankova@ctco.lv>
2018-09-05 16:10:45 +0300

    delete equals() and hashCode()

--- src/main/java/lv/ctco/javaschool/vote/entity/EventVote.java
+++ src/main/java/lv/ctco/javaschool/vote/entity/EventVote.java
@@ -63,28 +63,4 @@ public class EventVote {
     public void setComment(String comment) {
         this.comment = comment;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        EventVote eventVote = (EventVote) o;
-
-        if (id != null ? !id.equals(eventVote.id) : eventVote.id != null) return false;
-        if (event != null ? !event.equals(eventVote.event) : eventVote.event != null) return false;
-        if (user != null ? !user.equals(eventVote.user) : eventVote.user != null) return false;
-        if (mood != eventVote.mood) return false;
-        return comment != null ? comment.equals(eventVote.comment) : eventVote.comment == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (event != null ? event.hashCode() : 0);
-        result = 31 * result + (user != null ? user.hashCode() : 0);
-        result = 31 * result + (mood != null ? mood.hashCode() : 0);
-        result = 31 * result + (comment != null ? comment.hashCode() : 0);
-        return result;
-    }
 }
