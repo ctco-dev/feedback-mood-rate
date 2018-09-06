d23ffc97405f6561567b7c758b0fb87db670fed0
Gaaaach <gatislaizans95@gmail.com>
2018-09-06 11:39:07 +0300

    small clean and update

--- src/main/webapp/js/dayStatistics.js
+++ src/main/webapp/js/dayStatistics.js
@@ -47,18 +47,13 @@ function getStats() {
     var dayStats = document.getElementById("day-stats-radio");
     var weekStats = document.getElementById("week-stats-radio");
     if (dayStats.checked) {
-        console.log("Statistics type - Day");
         statsDto = {"date": document.getElementById("date-select").value};
     }
     else if (weekStats.checked) {
-        console.log("Statistics type - Week");
         statsDto = {
             "week": document.getElementById("week-select").value
         };
     }
-
-    console.log(statsDto);
-    console.log("Collecting Statistics from Database");
     fetch('/api/vote/getDailyStatistics', {
         "method": "POST",
         headers: {
